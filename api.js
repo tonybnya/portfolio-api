c16c92c5f8839cced7fef663325fc19cde16662c
tonybnya <nya.tony2010@gmail.com>
2024-05-25 11:21:30 +0100

    fix: connect the API to MongoDB Atlas

--- api.js
+++ api.js
@@ -1,23 +1,46 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+
+// require("dotenv").config();
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(bodyParser.json());
+
+// // Connection to MongoDB
+// mongoose.connect(process.env.MONGO_URI);
+
+// // Set routes
+// app.use("/projects", require("./routes/projects"));
+// app.use("/timeline", require("./routes/timeline"));
+
+// // Start and listening to the server
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-require("dotenv").config();
-
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Hello from API Server");
+});
+
+app.listen(3000);
 
 // Connection to MongoDB
-mongoose.connect(process.env.MONGO_URI);
-
-// Set routes
-app.use("/projects", require("./routes/projects"));
-app.use("/timeline", require("./routes/timeline"));
-
-// Start and listening to the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  // .connect(process.env.MONGO_URI)
+  .connect(
+    "mongodb+srv://tonybnya:H9aDF582zVBzR7bp@portfoliodb.e3aqjx8.mongodb.net/Portfolio-API?retryWrites=true&w=majority&appName=PortfolioDB",
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection Failed!");
+  });
